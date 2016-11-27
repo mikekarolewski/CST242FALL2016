99cbbede3ea7314e856dbd8f656d6ca8400bb16d
mikekarolewski <mike_karolewski@yahoo.com>
2016-11-26 19:28:48 -0500

    Create Customer.java

---
+++ NightClub/Model/Customer.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class Customer extends Person {
+
+	public Customer(int id, String firstName, String lastName, String email, String username, int monthOfBirth,
+			int dayOfBirth, int yearOfBirth) {
+
+		super(id, firstName, lastName, email, username, monthOfBirth, dayOfBirth, yearOfBirth);
+
+	}
+
+}
