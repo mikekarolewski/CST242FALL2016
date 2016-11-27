e3a4e4c16d7c2f82ea0e54a3b7a085637e3ff473
mikekarolewski <mike_karolewski@yahoo.com>
2016-11-26 19:29:11 -0500

    Create Manager.java

---
+++ NightClub/Model/Manager.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class Manager extends RegEmployee {
+	
+
+	public Manager(int id, String firstName, String lastName, String email, String username, int monthOfBirth,
+			int dayOfBirth, int yearOfBirth, String hireDate, String fireDate) {
+
+		super(id, firstName, lastName, email, username, monthOfBirth, dayOfBirth, yearOfBirth, hireDate, fireDate);
+
+	}
+
+}
