ca313e486749a1017a2a26c5404d74884c9f3507
mikekarolewski <mike_karolewski@yahoo.com>
2016-11-26 19:29:39 -0500

    Create Owner.java

---
+++ NightClub/Model/Owner.java
@@ -0,0 +1,11 @@
+package Model;
+
+public class Owner extends Person{
+
+	public Owner(int id, String firstName, String lastName, String email, String username, int monthOfBirth,
+			int dayOfBirth, int yearOfBirth) {
+		super(id, firstName, lastName, email, username, monthOfBirth, dayOfBirth, yearOfBirth);
+		
+	}
+
+}
